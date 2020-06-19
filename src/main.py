b5bf83e27551fc461de27b23c84378dc6de8a169
Juan Nunez <juanpa.nb@gmail.com>
2020-06-19 08:20:20 -0500

    Ahora siempre genera una contraseña segura

--- src/main.py
+++ src/main.py
@@ -1,12 +1,33 @@
 # Resolve the problem!!
 import string
+import random
 
 SYMBOLS = list('!"#$%&\'()*+,-./:;?@[]^_`{|}~')
+char_lower= list(string.ascii_lowercase)           #Se incluye el abecedario en minuscula y mayuscula junto con los digitos
+char_upper= list(string.ascii_uppercase) 
+char_digit= list(string.digits)
 
+
+def pass_sections(clist):
+    # Esta funcion me permite generar una seccion de entre 2 y 4 caracteres de la contraseña escogiendo una muestra de la lista ingresada
+    klen = random.randrange(2,5,2)
+    psection=random.sample(clist,k=klen)
+    return(psection)
 
 def generate_password():
     # Start coding here
-
+    # Genero una lista por cada seccion
+    plower= pass_sections(char_lower)
+    pupper= pass_sections(char_upper)
+    pnumber= pass_sections(char_digit)
+    psymbol= pass_sections(SYMBOLS)
+    # Uno las secciones y las desordeno usando shuffle
+    my_pass=(plower+pupper+pnumber+psymbol)
+    
+    random.shuffle(my_pass)
+    
+    return my_pass
+    
 
 def validate(password):
 
